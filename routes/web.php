d7ffc72d00a2db5b55606f18c311e410ed3626cb
Daniil Bykov <daniil.bykov@gmail.com>
2023-04-17 09:04:50 +0200

    Set up User controller with collection endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\UserCollection;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/users', function () {
+    return new UserCollection(User::paginate());
 });
